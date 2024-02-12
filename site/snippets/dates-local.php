930a299aa326efa5cac385ada4302734371c0647
Alex <alexrturner@gmail.com>
2024-02-12 10:52:14 +0800

    adjust local date and time style

--- site/snippets/dates-local.php
+++ site/snippets/dates-local.php
@@ -1,18 +1,37 @@
+<?php
+// fetch and format dates and times
+$start_date = $page->start_date()->toDate('F jS');
+$start_time = $page->start_time()->toDate('H:iA');
+
+$end_date = $page->end_date()->toDate('F jS');
+$end_time = $page->end_time()->toDate('H:iA');
+?>
+
 <ul class="dates items">
+
+    <li class="date">
+        <?= $start_date ?>
+        <?php if ($start_time) : ?>
+            <span class="time"><?= $start_time ?></span>
+        <?php endif; ?>
+
+        <?php
+        // if end date and start date are the same, display different times
+        if ($end_date && $start_date === $end_date) {
+            if ($end_time && $start_time !== $end_time) {
+                echo " – " . $end_time;
+            }
+        }
+        ?>
+    </li>
     <?php
-    if ($start_date = $page->start_date()->toDate('F jS')) : ?>
-        <li class="date">
-            <?= $start_date ?>
-            <?php if ($start_time = $page->start_time()->toDate('H:iA')) : ?>
-                <span class="time">, <?= $start_time ?></span>
-            <?php endif; ?>
-        </li>
-    <?php endif; ?>
-    <?php if ($end_date = $page->end_date()->toDate('F jS')) : ?>
+    // if end date is different from start date
+    if ($end_date && $start_date !== $end_date) : ?>
         <li class="date"><?= $end_date ?>
-            <?php if ($end_time = $page->end_time()->toDate('H:iA')) : ?>
-                <span class="time">, <?= $end_time ?></span>
+            <?php if ($end_time) : ?>
+                <span class="time"><?= $end_time ?></span>
             <?php endif; ?>
         </li>
     <?php endif; ?>
+
 </ul>
