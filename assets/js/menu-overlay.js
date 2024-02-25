166a6f76f2a41786ae56baca0a65cc9e615d9488
Alex <alexrturner@gmail.com>
2024-02-26 03:13:54 +0800

    Update menu overlay for mobile

--- assets/js/menu-overlay.js
+++ assets/js/menu-overlay.js
@@ -9,5 +9,11 @@ document.addEventListener("DOMContentLoaded", function () {
     overlay.style.opacity = isOpen ? "0" : "1";
     overlay.style.visibility = isOpen ? "hidden" : "visible";
     menu.classList.toggle("hidden");
+    // toggle body scroll
+    if (isOpen) {
+      document.body.style.overflowY = "scroll";
+    } else {
+      document.body.style.overflowY = "hidden";
+    }
   });
 });
